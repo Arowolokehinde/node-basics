3fb8de059568eaba8fe2cd0b825c1fb9143e1a87
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-10 14:33:26 +0100

    node-basics

--- index.js
+++ index.js
@@ -60,44 +60,43 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/templates-product.html`,
   "utf-8"
 );
-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(url.parse(req.url, true));
   const pathName = req.url;
 
-  //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, { "Content-type": "text.html" });
+    res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
+
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
 
     // Product page
   } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+    res.end(`This is the PRODUCT`);
 
     // API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
-    // Not Found
+    // NOT FOUND
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello-world",
     });
-    res.end("Page not found");
+    res.end(`Page not found!`);
   }
+
+  // res.end("Hello from the server!");
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests n port 8000");
+  console.log(`Listening to requests on port 8000`);
 });
